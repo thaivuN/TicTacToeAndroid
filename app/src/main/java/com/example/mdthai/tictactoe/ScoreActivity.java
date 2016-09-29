78dbe1dd1bca52e01333e58d470713cac05ab8fd
Thai-Vu Nguyen <thaivu22@gmail.com>
2016-09-29 16:01:55 -0400

    Updated Class and Layout for Help and Score

--- app/src/main/java/com/example/mdthai/tictactoe/ScoreActivity.java
+++ app/src/main/java/com/example/mdthai/tictactoe/ScoreActivity.java
@@ -2,6 +2,7 @@ package com.example.mdthai.tictactoe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by MDThai on 9/29/2016.
@@ -13,5 +14,13 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scoreactivity_main);
+    }
+
+    /**
+     * Closes the activity
+     * @param view
+     */
+    public void onBtnFinish(View view){
+        super.finish();
     }
 }
